88366800316358decf5a752a56be3c563167f397
Tim Culverhouse <tim@timculverhouse.com>
2025-12-29 15:42:08 -0600

    fix: use std.fs.File.stdin for Linux compatibility

--- src/platform/linux.zig
+++ src/platform/linux.zig
@@ -121,7 +121,7 @@ pub fn countFile(path: []const u8, mode: CountMode) FileResult {
 
 /// Read from stdin
 pub fn countStdin(mode: CountMode) Counts {
-    const stdin = std.io.getStdIn();
+    const stdin = std.fs.File.stdin();
     var buf: [READ_BUF_SIZE]u8 = undefined;
     var total = Counts{};
     var in_word = false;
